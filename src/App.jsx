4e840d922967dc3196246d859f58d6166f8affc3
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-02 01:32:25 +0100

    Create Hero and update styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
+export default function App(){
+    return(
+        <div className="app">
+            <Navbar />
+            <Hero />
+        </div>
+    )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <div className="hero">
+            <img className="hero--image" src="/images/photo-grid.png"/>
+            <h1 className="hero--title">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
