a87bdd43a8b352633572a14f4469640f95562c3f
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-06 05:18:24 +0100

    Page Complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero(){
     return(
         <div className="hero">
-            <img className="hero--image" src="/images/photo-grid.png"/>
+            <img className="hero--image" src="images/photo-grid.png"/>
             <h1 className="hero--title">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </div>
