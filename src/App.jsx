963c8ea6daca917600fce3586e15b54949722963
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-06 03:18:48 +0100

    Working props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App(){
     return(
         <div className="app">
             <Navbar />
             <Hero />
+            <Card 
+                image="kz.png"
+                rating="5.0"
+                reviews="6"
+                country="USA"
+                price="136"
+                title="Life lessons with Katie Zaferes"
+            />
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(){
     return(
         <nav className="nav">
-            <img className="nav--logo" src="/images/airbnb-logo.png"></img>
+            <img className="nav--logo" src="/images/airbnb-logo.png"/>
         </nav>
     )
 }
